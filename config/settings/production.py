f231956eb6f0d14c5fd5b1c693468d07875a3892
NickSheva <nikshevtsov@mail.ru>
2025-03-06 18:14:27 +0500

    changed production

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,14 +1,15 @@
-from .base import *
 import environ
-
+from config.settings.base import BASE_DIR  # Явный импорт BASE_DIR
 
 # Инициализация django-environ
 env = environ.Env()
-env.read_env('.env')
+env.read_env(".env")
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG")
+
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
 DATABASES = {
     "default": {
         "ENGINE": env("DB_ENGINE"),
@@ -19,4 +20,9 @@ DATABASES = {
         "PORT": env("DB_PORT"),
     }
 }
+
+# Настройки статики
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Отключение предупреждений в VS Code
+# pylint: disable=wildcard-import, unused-wildcard-import
