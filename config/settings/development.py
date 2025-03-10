f93fc1342afd572e2bd3f48870e06129183a16a0
NickSheva <nikshevtsov@mail.ru>
2025-03-10 18:57:03 +0500

    change

--- config/settings/development.py
+++ config/settings/development.py
@@ -21,6 +21,12 @@ DATABASES = {
         "PORT": env("DB_PORT"),
     }
 }
-
+# Настройки для отправки email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
 # Настройки для отправки email (например, вывод в консоль)
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
