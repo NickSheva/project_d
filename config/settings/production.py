4a8c734069c35c1930e599b95726f3d1449c083f
NickSheva <nikshevtsov@mail.ru>
2025-03-15 14:33:45 +0500

    add production

---
+++ config/settings/production.py
@@ -0,0 +1,25 @@
+from .base import *
+import environ
+
+
+env = environ.Env()
+env.read_env('.env')
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = False
+# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+import os
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+DATABASES = {'default': {
+    'ENGINE': env('DB_ENGINE'),
+    'NAME': env('DB_NAME'),
+    'USER': env('DB_USER'),
+    'PASSWORD': env('DB_PASSWORD'),
+    'HOST': env('DB_HOST'),
+    'PORT': env('DB_PORT'),
+    }
+}
+# Настройки статических файлов
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
